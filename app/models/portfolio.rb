ea61ee1cd89145f207768170fa1f02f232e8ca5f
Robin Neu <robin.neu@devinci.fr>
2019-06-03 20:33:05 +0200

    Integrated nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,10 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies, 
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
+#plusieurs atttributs, mais rejette si le nom est blanc/vide 
+#technologies_attributes:[{},{},{}]  key/value pairs ensemble de paires entre clé et valeurs => hash
+
   include Placeholder
 
   validates_presence_of :title, :body, :main_image, :thumb_image
@@ -13,10 +18,10 @@ class Portfolio < ApplicationRecord
 
   def set_defaults
   	self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-	self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+	  self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
-
+# pareil que => 
 # if self.main_image = nil
 # 	self.main_image = "https://via.placeholder.com/600x400"
 # end		
