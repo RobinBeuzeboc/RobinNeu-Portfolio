6b909f655f84a4fbb6a61a98c4db23d54ae772b1
Robin Neu <robin.neu@devinci.fr>
2019-06-06 20:37:57 +0200

    Code refactoring for portfolio

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -79,7 +79,9 @@ class BlogsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
 
-      params.require(:blog).permit(:title, :body)
+    #  params.require(:blog).permit(:title, :body) si pas setup dans application.rb dans config
+      params.require(:blog)
+
     end
 
     
