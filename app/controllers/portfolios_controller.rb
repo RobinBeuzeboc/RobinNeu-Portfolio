65ce0ac995626afdb1172c7f420dc7baae7241a0
Robin Neu <robin.neu@devinci.fr>
2019-06-10 18:20:30 +0200

    Refactored action for Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,5 @@
 class PortfoliosController < ApplicationController
-
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
   layout "portfolio"
 
 
@@ -18,7 +18,6 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -30,13 +29,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
-
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-    
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -48,12 +43,11 @@ class PortfoliosController < ApplicationController
 
 
   def show
-  	@portfolio_item = Portfolio.find(params[:id])
   end 
+
   
   def destroy
     # Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
 
     # Destroy/delete the record
     @portfolio_item.destroy
@@ -66,6 +60,11 @@ class PortfoliosController < ApplicationController
   end
 
     private #attrignablle uniquement dans ce controlleur
+
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+
     def portfolio_params
       params.require(:portfolio).permit(:title, 
                                         :subtitle, 
