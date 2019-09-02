c8cc737f18447ee1a87d37193e2b8017a4e0cb18
Robin Neu <robin.neu@devinci.fr>
2019-09-02 12:26:03 +0200

    Implemented initial commit and Twitter gem

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ Bundler.require(*Rails.groups)
 
 module DevPortfolio
   class Application < Rails::Application
-
+    config.eager_load_paths << "#{Rails.root}/lib" 
     config.action_controller.permit_all_parameters = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
