8b498cecac007c183c4be4e1e4840d4e3ec265c4
Robin Neu <robin.neu@devinci.fr>
2019-09-07 22:06:42 +0200

    Integrated prod calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_cable.allowed_request_origins = ['https://rnkjaald-portfolio.herokuapp.com/', 'http://rnkjaald-portfolio.herokuapp.com/']
+  config.action_cable.url = "wss://devcamp-portfolio.herokuapp.com/cable"
 end
