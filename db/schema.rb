983a54296a92d4f1c377dd0ba00d65b2c4a3b38a
Robin Neu <robin.neu@devinci.fr>
2019-09-07 21:05:25 +0200

    db

--- db/schema.rb
+++ db/schema.rb
@@ -95,7 +95,6 @@ ActiveRecord::Schema.define(version: 2019_09_07_185405) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "roles"
     t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
